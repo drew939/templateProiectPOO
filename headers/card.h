df665289f0436ab66e52afa932a510bfa4bcc416
drew939 <andrei.enache916@gmail.com>
2026-04-01 10:47:56 +0300

    ...

--- headers/card.h
+++ headers/card.h
@@ -2,14 +2,17 @@ class CardBancar{
     private:
         bool activat = false;
         bool blocat = false;
+
         std::string titularCard;
         std::string valuta;
         double limitaTranzactii;
+
         std::string dataExpirare;
         std::string numarCard;
         std::string numarSecuritate;
         std::string codPin;
         std::string operatorPlati;
+
     public:
         CardBancar(std::string _valuta, std::string _operator) : valuta(_valuta), operatorPlati(_operator){} 
         
@@ -18,22 +21,24 @@ class CardBancar{
 class CardDebit : virtual public CardBancar{
     private:
         double limitaRetragere;
+        bool platiInternationale;
 
 };
 
 class CardCredit : virtual public CardBancar{
     private:
         double limitaCredit;
+        double dobanda;
 };
 
 class CardOverdraft : virtual public CardDebit, virtual public CardCredit{
-
+    double taxaDepasireLimita;
 };
 
 class CardVirtual : virtual public CardDebit{
-
+    bool singleUse;
 };
 
 class CardPrepaid : virtual public CardVirtual{
-
+    double limitaIncarcareLunara;
 };

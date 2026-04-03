29e440236bcc72280577645d14a05fc75a8af427
drew939 <andrei.enache916@gmail.com>
2026-04-03 05:42:37 +0300

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include "../headers/card.h"
-#include "../headers/client.h"
-#include "../headers/cont.h"
-#include "../headers/imprumut.h"
-#include "../headers/investitii.h"
+#include "../headers/meniu.h"
+
+int main() 
+{
+    Meniu& meniu = Meniu::getInstance();
+    meniu.run();
+    return 0;
+}
